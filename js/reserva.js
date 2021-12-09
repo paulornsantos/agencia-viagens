91eb6c18f1e804363d7c7de4e1532d8fac764c6a
Paulo Santos <paulornsantos96@gmail.com>
2021-12-09 18:01:52 -0300

    Página reservas

---
+++ js/reserva.js
@@ -0,0 +1,11 @@
+
+var botao = document.getElementById("reserva");
+
+function reservar(){
+    console.log("cheguei aqui pai");
+    alert("Reserva efetuada com sucesso, verefique seu e-mail para mais informações")
+}
+
+
+
+botao.addEventListener("click", reservar);
